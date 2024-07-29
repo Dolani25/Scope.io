04b0fe488de6cfabb0d99f35ce9af2fcece6b497
Dolani25 <akinmarvelous2030@gmail.com>
2024-07-29 11:18:41 +0100

    mehn

--- scope/settings.py
+++ scope/settings.py
@@ -77,16 +77,12 @@ WSGI_APPLICATION = 'scope.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
  
- DATABASES = { 
- 'default' : { 
- 'ENGINE': 'django.db.backends.postgresql', 
- 'NAME':'railway', 
- 'USER':'postgres', 
- 'PASSWORD':'GzCkMixulXwWnIPYlkex', 
-'HOST':'containers-us-west-92.railway.app', 
- 'PORT':'5526', 
-} 
- } 
+ DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
