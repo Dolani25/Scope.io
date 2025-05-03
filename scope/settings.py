5625c9096b478d55497ed85ca9676326f31d076b
Dolani <42421068+Dolani25@users.noreply.github.com>
2025-05-03 08:28:00 +0100

    Update settings.py

--- scope/settings.py
+++ scope/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'scope.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
  
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
- }
+}
 
 
 # Password validation
