a677aa2c8f90f00e4be5786af81a25dec409cd9d
Dolani <42421068+Dolani25@users.noreply.github.com>
2025-07-24 08:58:20 +0100

    Update urls.py

--- scope/urls.py
+++ scope/urls.py
@@ -32,8 +32,10 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL , document_root= settings.MEDIA_ROOT)
 
  
- urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
- urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 '''
     path('signup' , signup, name='signup'),
